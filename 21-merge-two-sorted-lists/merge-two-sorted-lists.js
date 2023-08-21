f8c15881a5557bcd0f9beeb3a61245112045517a
Benjamin Ortiz <92768343+Benjamin-Ortiz@users.noreply.github.com>
2023-08-21 00:08:08 -0400

    Time: 69 ms (34.75%) | Memory: 43.4 MB (96.41%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+        Merge the two lists into one sorted list. 
+        The list should be made by splicing together 
+                 the nodes of the first two lists.
+        Return the head of the merged linked list.
+
+        I:
+        O:
+ */
+const mergeTwoLists = function (l1, l2) {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
+};
