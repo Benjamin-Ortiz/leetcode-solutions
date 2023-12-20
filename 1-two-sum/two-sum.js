5fb78d2fffbc03f62b52ea9b5ab77f6df2c7d9ac
Benjamin Ortiz <92768343+Benjamin-Ortiz@users.noreply.github.com>
2023-12-20 12:31:26 -0500

    Time: 84 ms (39.58%) | Memory: 42.5 MB (53.33%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,12 +6,13 @@
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         let n1 = nums[i];
+        // console.log(n1, " i = ", i)
 
         for (let j = i + 1; j < nums.length; j++) {
             let n2 = nums[j];
+            // console.log(n2, " j = ", j) 
 
             if (n1 + n2 === target) {
-                console.log(i,j)
             return [i,j];
             }
         }
